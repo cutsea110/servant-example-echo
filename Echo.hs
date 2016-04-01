85fd9c35a3eae7b618e71b98652c139564a07b83
cutsea110 <cutsea110@gmail.com>
2016-04-01 10:17:18 +0900

    init

---
+++ Echo.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+module Echo where
+
+import Servant
+import Network.Wai (Application)
+import Network.Wai.Handler.Warp (run)
+
+type Echo = "echo" :> ReqBody '[PlainText] String :> Get '[PlainText] String
+
+handleEcho :: Server Echo
+handleEcho txt = return txt
+
+api :: Proxy Echo
+api = Proxy
+
+app :: Application
+app = serve api handleEcho
+
+-- | $ cabal exec -- runhaskell Echo.hs
+--   and enter next command in other shell prompt.
+--   $ curl -X GET -H 'Content-Type: text/plain;charset=utf-8' -d 'Hello' http://localhost:8081/echo
+main :: IO ()
+main = run 8081 app
